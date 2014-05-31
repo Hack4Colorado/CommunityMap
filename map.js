a88109337e4fa617b9d463888132f7ba417f1bd7
Nate Watts <nathanielrwatts@gmail.com>
2014-05-31 11:11:58 -0600

    Added changes to Maps

--- map.js
+++ map.js
@@ -1,11 +1,13 @@
 var map;
 function initialize() {
-  var mapOptions = {
+  // Create a simple map.
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 10,
     center: new google.maps.LatLng(39.7392, -104.9847)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  });
+
+  // Load a GeoJSON from the same server as our demo.
+  map.data.loadGeoJson('https://github.com/johan/world.geo.json/blob/master/countries/USA/CO/Jefferson.geo.json');
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
